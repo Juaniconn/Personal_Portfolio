e758e236c4b56a60640bcb09c7e4306e30a40b36
Juan Francisco Ponce Luna <ElJuanii@hotmail.com>
2019-04-13 13:58:10 -0500

    finishing navigation bar

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "rules":{
     "block-no-empty": true,
-    "unit-whitelist": ["em", "rem", "%", "s", "px", "fr", "vh", "dpi", "x", "deg"],
+    "unit-whitelist": ["em", "rem", "%", "s", "px", "fr", "vh", "vw", "dpi", "x", "deg"],
   }
 }
